ab49bba088d55c961b1b5e5bf4280d51b2b746e3
CunhazadaNoDale <cunha12324@gmail.com>
2026-01-09 21:03:44 -0300

    feat: query adicionada para buscar o carrinho pelo usuario logado

--- src/main/java/testando/indiano/repositories/CartRepository.java
+++ src/main/java/testando/indiano/repositories/CartRepository.java
@@ -1,9 +1,13 @@
 package testando.indiano.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import testando.indiano.model.Cart;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    @Query("SELECT c FROM Cart c WHERE c.user.email = ?1")
+    Cart findCartByEmail(String email);
 }
