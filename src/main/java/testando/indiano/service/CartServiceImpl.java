327941a317c48d3102f3c26fc204adcbedcf8329
CunhazadaNoDale <cunha12324@gmail.com>
2026-01-09 21:04:16 -0300

    feat: adição do service implementando a interface

--- src/main/java/testando/indiano/service/CartServiceImpl.java
+++ src/main/java/testando/indiano/service/CartServiceImpl.java
@@ -1,4 +1,51 @@
 package testando.indiano.service;
 
-public class CartServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import testando.indiano.exceptions.ResourceNotFoundException;
+import testando.indiano.model.Cart;
+import testando.indiano.model.Product;
+import testando.indiano.payload.CartDTO;
+import testando.indiano.repositories.CartRepository;
+import testando.indiano.repositories.ProductRepository;
+
+@Service
+public class CartServiceImpl implements CartService{
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private AuthUtil authUtil;
+
+    @Override
+    public CartDTO addProductToCart(Long productId, Integer quantity) {
+        Cart cart = createCart();
+
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("Product", "productId", productId));
+
+
+
+
+        return null;
+    }
+
+    private Cart createCart(){
+        Cart userCart = cartRepository.findCartByEmail(authUtil.loggerInEmail());
+
+        if(userCart != null) {
+            return userCart;
+        }
+
+        Cart cart = new Cart();
+        cart.setTotalPrice(0.0);
+        cart.setUser(authUtil.loggedInUser());
+        Cart newCart = cartRepository.save(cart);
+
+        return newCart;
+    }
 }
