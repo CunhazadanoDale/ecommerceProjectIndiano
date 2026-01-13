97dfe504270252ec651f0ab92304d30585a74400
CunhazadaNoDale <cunha12324@gmail.com>
2026-01-12 22:21:23 -0300

    feat: adição de query para buscar por id produto e carrinho

--- src/main/java/testando/indiano/repositories/CartItemRepository.java
+++ src/main/java/testando/indiano/repositories/CartItemRepository.java
@@ -1,7 +1,11 @@
 package testando.indiano.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import testando.indiano.model.CartItem;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    @Query("SELECT ci FROM CartItem ci WHERE ci.cart.id = ?1 AND ci.product.id = ?2")
+    CartItem findCartItemByProductIdAndCartId(Long cartId, Long productId);
 }
