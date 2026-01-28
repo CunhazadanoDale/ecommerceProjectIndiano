90ce263c52ba54ca837beaeea4e33a0d894f197e
CunhazadaNoDale <cunha12324@gmail.com>
2026-01-27 23:06:09 -0300

    feat: adição de metodo no service para atualizar carrinho

--- src/main/java/testando/indiano/service/CartService.java
+++ src/main/java/testando/indiano/service/CartService.java
@@ -1,5 +1,6 @@
 package testando.indiano.service;
 
+import jakarta.transaction.Transactional;
 import testando.indiano.payload.CartDTO;
 
 import java.util.List;
@@ -8,6 +9,8 @@ import java.util.List;
 public interface CartService {
     CartDTO addProductToCart(Long productId, Integer quantity);
     List<CartDTO> getAllCarts();
-
     CartDTO getCart(String emailId, Long cartId);
+
+    @Transactional
+    CartDTO updateProductQuantityInCart(Long productId, Integer quantity);
 }
