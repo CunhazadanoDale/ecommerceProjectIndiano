e2b7d841fe1e82a7a834172218f61378b8e5ffaa
CunhazadaNoDale <cunha12324@gmail.com>
2026-03-18 21:19:44 -0300

    feat: adição da service implementadora

--- src/main/java/testando/indiano/service/AddressServiceImpl.java
+++ src/main/java/testando/indiano/service/AddressServiceImpl.java
@@ -1,4 +1,37 @@
 package testando.indiano.service;
 
-public class AddressServiceImpl {
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import testando.indiano.model.Address;
+import testando.indiano.model.User;
+import testando.indiano.payload.AddressDTO;
+import testando.indiano.repositories.AddressRepository;
+import testando.indiano.util.AuthUtil;
+
+import java.util.List;
+
+@Service
+public class AddressServiceImpl implements AddressService{
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+    @Override
+    public AddressDTO createAddress(AddressDTO addressDTO, User user) {
+
+        Address address = modelMapper.map(addressDTO, Address.class);
+        List<Address> addressList = user.getAddresses();
+
+        addressList.add(address);
+        user.setAddresses(addressList);
+
+        address.setUser(user);
+        Address savedAddress = addressRepository.save(address);
+
+        return modelMapper.map(savedAddress, AddressDTO.class);
+    }
 }
