e0d38db9c2997b5052b840be2641fbabd49457a6
CunhazadaNoDale <cunha12324@gmail.com>
2026-03-18 21:18:20 -0300

    feat: adição do controller para criar o endereço

---
+++ src/main/java/testando/indiano/controller/AddressController.java
@@ -0,0 +1,31 @@
+package testando.indiano.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import testando.indiano.model.User;
+import testando.indiano.payload.AddressDTO;
+import testando.indiano.service.AddressService;
+import testando.indiano.util.AuthUtil;
+
+@RestController
+@RequestMapping("/api")
+public class AddressController {
+
+    @Autowired
+    private AddressService addressService;
+
+    @Autowired
+    private AuthUtil authUtil;
+
+    @PostMapping("/addresses")
+    public ResponseEntity<AddressDTO> createAddress(AddressDTO addressDTO) {
+        User user = authUtil.loggedInUser();
+        AddressDTO savedAddressDTO = addressService.createAddress(addressDTO, user);
+
+        return new ResponseEntity<>(savedAddressDTO, HttpStatus.CREATED);
+    }
+}
