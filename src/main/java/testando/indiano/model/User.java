402513f40c67bc320c9ed0e5df543447fa6030b7
CunhazadaNoDale <cunha12324@gmail.com>
2026-03-18 21:19:04 -0300

    fix: correção relacionamento para one to many com endereços

--- src/main/java/testando/indiano/model/User.java
+++ src/main/java/testando/indiano/model/User.java
@@ -58,7 +58,7 @@ public class User {
 
     @Getter
     @Setter
-    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
     @JoinTable(name = "user_address",
                 joinColumns = @JoinColumn(name = "user_id"))
     private List<Address> addresses = new ArrayList<>();
