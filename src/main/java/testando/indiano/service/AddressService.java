7e7b4d730939305b9e03330ec5793caa73876580
CunhazadaNoDale <cunha12324@gmail.com>
2026-03-18 21:19:21 -0300

    feat: adição dos serviços de endereço

--- src/main/java/testando/indiano/service/AddressService.java
+++ src/main/java/testando/indiano/service/AddressService.java
@@ -1,4 +1,8 @@
 package testando.indiano.service;
 
+import testando.indiano.model.User;
+import testando.indiano.payload.AddressDTO;
+
 public interface AddressService {
+    AddressDTO createAddress(AddressDTO addressDTO, User user);
 }
