6e41998547393088b85e81393ce017a570bb2d95
CunhazadaNoDale <cunha12324@gmail.com>
2026-03-23 19:43:12 -0300

    feat: adição de métodos na interface do service

--- src/main/java/testando/indiano/service/AddressService.java
+++ src/main/java/testando/indiano/service/AddressService.java
@@ -3,6 +3,10 @@ package testando.indiano.service;
 import testando.indiano.model.User;
 import testando.indiano.payload.AddressDTO;
 
+import java.util.List;
+
 public interface AddressService {
     AddressDTO createAddress(AddressDTO addressDTO, User user);
+    List<AddressDTO> getAddress(User user);
+    List<AddressDTO> getAddresses();
 }
